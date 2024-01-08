bcdc402f6819e07211b3ac4c04c6389615a9122d
adithyashibu <adithyas447@gmail.com>
2024-01-08 12:48:49 +0530

    Trial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
+
+const App = () => {
+  return (
+    <div className="min-h-screen"> 
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <div className="white-glassmorphism">
+        <Services />
+        <Transactions />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+ 
+export default App;
